b17eddd2e607ebf50f895e31c1a79354007d4d66
SaiPavanN01 <88645382+SaiPavanN01@users.noreply.github.com>
2025-03-19 13:13:04 +0530

    Added Main

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr={2,4,6,9,11,12,14,18,19};
+        int target=14;
+        System.out.println(binarySearch(arr,target));
+    }
+    static int binarySearch(int[] arr, int target){
+       int start=0;
+       int end = arr.length-1;
+       while(start <= end){
+           int mid = start +(end-start)/2;
+           if(arr[mid] ==target)
+               return mid;
+           if(arr[mid]>target)
+               end=mid-1;
+           if(arr[mid] < target)
+               start=mid+1;
+       }
+       return -1;
+    }
+}
